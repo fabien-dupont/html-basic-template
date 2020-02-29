b7a1b0291d7d0f4da49f25c5dbd38bb421b140e3
Fabien Dupont <fab.d1987@yandex.com>
2020-02-29 18:27:39 +0100

    Update app.js

--- app.js
+++ app.js
@@ -9,8 +9,7 @@ function buttonClickGET(){
   var queryLoc = document.getElementById("queryLoc").value;
   var apikey = "f9fhsIyi2W9sBfBUskjY4kl5S6W7HE7xLnth6Md49AOoiSf4FLkLjriPyenljIgR"
   
-  //var url = "https://api.openweathermap.org/data/2.5/weather?q="+queryLoc+"&appid=c21a75b667d6f7abb81f118dcf8d4611&units=metric" ;
-  var url = "https://orion.apiseeds.com/api/music/search/?q="+queryLoc+"&apikey="+apikey ;
+  var url = "https://apiprovider.com/api/dir/search/?q="+queryLoc+"&apikey="+apikey ;
   $.get(url, callBackGetSuccess).done(function() {
     console.log("api callback success");
   })
